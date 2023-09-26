72791a8d6b5f40ee589cac86c214e3f3ea4f1c95
Anthahkarana <100427124+githubber-me@users.noreply.github.com>
2023-09-26 16:24:22 +0530

    FACTORIAL OF A NUMBER

---
+++ learn2.py
@@ -0,0 +1,11 @@
+#Factorial Number
+num = int(input("Enter a number: "))
+fact = 1
+if num < 0:
+   print("Enter a positive value")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       fact = fact*i
+   print("The factorial of",num,"is",fact)
